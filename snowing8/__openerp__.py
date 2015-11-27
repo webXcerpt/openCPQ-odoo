f5089f62b3ef8b2531d1810b1382fdec458ebba5
Heribert Schütz <hs@webxcerpt.com>
2015-11-27 15:31:40 +0100

    minor change, just to test the work flow

--- snowing8/__openerp__.py
+++ snowing8/__openerp__.py
@@ -7,7 +7,7 @@
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Long description of module's purpose
+        Very long description of module's purpose
     """,
 
     'author': "My Company",
