c8eb3527b09a58207baf2eb2441a32718dba10c5
Sebastian Schmidt <sebastian.andi.schmidt@googlemail.com>
2015-11-30 21:29:34 +0000

    Adding two dummy-fields to the sales order line views

--- opencpq/models.py
+++ opencpq/models.py
@@ -13,3 +13,10 @@ from openerp import models, fields, api
 #     @api.depends('value')
 #     def _value_pc(self):
 #         self.value2 = float(self.value) / 100
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    opencpq_url = fields.Char(string='OpenCPQ URL')
+    opencpq_json = fields.Char(string='OpenCPQ JSON')
+    
