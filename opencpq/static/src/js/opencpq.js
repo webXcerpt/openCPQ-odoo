51bcc843a56620f0b92d52189a9e33134ce3f510
Sebastian Schmidt <sebastian.andi.schmidt@googlemail.com>
2015-12-04 05:10:16 +0000

    Add additional new static folders missing in the previous commit

---
+++ opencpq/static/src/js/opencpq.js
@@ -0,0 +1,40 @@
+odoo.define('sale.order.line', function (require) {
+"use strict";
+
+var core = require('web.core');
+var data = require('web.data');
+var form_common = require('web.form_common');
+var formats = require('web.formats');
+var Model = require('web.DataModel');
+var time = require('web.time');
+var utils = require('web.utils');
+
+var QWeb = core.qweb;
+var _t = core._t;
+
+var ProductConfigurator = form_common.FormWidget.extend({
+    /* Approach - 
+	start: function() {
+        this.$el.append("<div>Test!</div>");
+        this.$el.append('<div><iframe src="/opencpq/static/optical-transport/index.html" width="100%" height="100%" marginwidth="0" marginheight="0" frameborder="no" scrolling="no" style="border-width:0px;"> </iframe></div>');
+    },
+    */
+	
+	start: function() {
+        this._super();
+        this.field_manager.on("field_changed:opencpq_configurator", this, this.display_configurator);
+        this.display_configurator();
+    },
+    
+    display_configurator: function() {
+        this.$el.html(QWeb.render("WidgetConfigurator", {
+            "configurator": this.field_manager.get_field_value("opencpq_configurator") || 0,
+        }));
+    }
+    
+    
+});
+
+core.form_custom_registry.add('product_configurator', ProductConfigurator);
+
+});
