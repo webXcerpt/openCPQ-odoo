a9763f31da99c3ab63617f07174c850c8e0f21b9
Sebastian Schmidt <sebastian.andi.schmidt@googlemail.com>
2015-12-09 17:47:54 +0000

    Inherit and change the product_variant_easy_edit_view as well

--- product_opencpq/models.py
+++ product_opencpq/models.py
@@ -7,7 +7,7 @@ class product_template(osv.osv):
     _inherit = 'product.template'
     _columns = {
         'configurator_ok': fields.boolean('Enable the openCPQ Product Configurator', help='Determine if a product can be configured with the openCPQ product configurator.'),
-        'configurator_type': fields.char('Type of Configurator', help='Type a valid configurator name. It must be the same name as the Folder name in the static directory'),    
+        'configurator_type': fields.char('Type of Configurator', help='Type a valid configurator name. It must be the same name as the folder name in the static directory'),    
     }
     
 class product_product(osv.osv):
