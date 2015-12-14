92e404abfeb4fe3c1b41a01bca037f2adb63143e
Sebastian Schmidt <sebastian.andi.schmidt@googlemail.com>
2015-12-14 19:48:52 +0100

    Adapt UI to the new specification
    Change the product template and product variant views and models

--- product_opencpq/models.py
+++ product_opencpq/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-''' Old API ''' 
+''' Old API '''
 
 from openerp import api
 from openerp.osv import fields, osv
@@ -9,13 +9,13 @@ class product_template(osv.osv):
     _inherit = 'product.template'
     _columns = {
         'configurator_ok': fields.boolean('Enable the openCPQ Product Configurator', help='Determine if a product can be configured with the openCPQ product configurator.'),
-        'configurator_type': fields.char('Type of Configurator', help='Type a valid configurator name. It must be the same name as the folder name in the static directory'),    
+        'configurator_type': fields.char('Type of Configurator', help='Type a valid configurator name. It must be the same name as the folder name in the static directory'),
     }
-    
+
 class product_product(osv.osv):
     _inherit = 'product.product'
     _columns = {
-        'configuration_result': fields.text('Configuraton', help='This is the result of the configuration on the corresponding template'),        
-    }
+        'template_ok': fields.boolean('Use an openCPQ-template', help='...'),
+        'configuration_result': fields.text('Configuraton', help='This is the result of the configuration on the corresponding template'),
 
-    
+    }
