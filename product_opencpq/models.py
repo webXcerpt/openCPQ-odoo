82178330443e70e6c29a3e269877c41aa6feca16
Heribert Schütz <hs@webxcerpt.com>
2015-12-16 15:34:34 +0100

    Shortened field name "Type of Configurator" to "Configurator"

--- product_opencpq/models.py
+++ product_opencpq/models.py
@@ -9,7 +9,7 @@ class product_template(osv.osv):
     _inherit = 'product.template'
     _columns = {
         'configurator_ok': fields.boolean('Enable the openCPQ Product Configurator', help='Determine if a product can be configured with the openCPQ product configurator. Cannot be changed after creation.'),
-        'configurator_type': fields.char('Type of Configurator', help='Type a valid configurator name. It must be the same name as the folder name in the static directory'),
+        'configurator_type': fields.char('Configurator', help='Enter a valid configurator name. It must be the same name as the folder name in the static directory'),
     }
 
     def create_variant_ids(self, cr, uid, ids, context=None):
