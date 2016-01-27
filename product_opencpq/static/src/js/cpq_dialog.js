b77ec8cf217802b34524ae8bb9757fe119480fa2
Sebastian Schmidt <sebastian.andi.schmidt@googlemail.com>
2016-01-27 18:38:47 +0100

    Refactored configuration dialog and display

--- product_opencpq/static/src/js/cpq_dialog.js
+++ product_opencpq/static/src/js/cpq_dialog.js
@@ -1,16 +1,20 @@
-odoo.define('product.product', function (require) {
+odoo.define('product_opencpq.configuration_dialog', function (require) {
 "use strict";
 
 var core = require('web.core');
-var data = require('web.data');
 var Dialog = require('web.Dialog');
 var form_common = require('web.form_common');
-var formats = require('web.formats');
-var Model = require('web.DataModel');
-var utils = require('web.utils');
+require('web.DataModel'); //not sure why necessary
 
 var QWeb = core.qweb;
 var _t = core._t;
+
+var FullscreenDialog = Dialog.extend({
+    init: function (parent, options) {
+        this._super.apply(this, arguments);
+        this.$modal.find('.modal-dialog').addClass('modal-fullscreen');
+    }
+});
 
 var ConfigurationDialog = form_common.FormWidget.extend({
 
@@ -20,28 +24,31 @@ var ConfigurationDialog = form_common.FormWidget.extend({
 
 	start: function() {
         this._super();
-        this.display_dialogbutton();
+        this.$el.html(QWeb.render("opencpq_configure", {}));
+		this.display_html();
+		this.field_manager.on("field_changed:configuration_html", this, this.display_html);
+
+        this.on("change:effective_readonly", this, this.check_readonly);
+        this.check_readonly();
+	},
+
+    check_readonly: function() {
+        var ro = this.get("effective_readonly");
+        this.$el.find(".opencpq_button_class").prop('disabled', ro);
     },
 
-    display_dialogbutton: function() {
-        this.$el.html(QWeb.render("opencpq_button", {
-        }));
-    },
-
-    display_configurator: function() {
-        this.$el.html(QWeb.render("WidgetConfigurator", {
-            "configurator": this.field_manager.get_field_value("configurator_type") || 0,
-        }));
-    },
+	display_html: function() {
+		var html = this.field_manager.get_field_value("configuration_html");
+		this.$el.find(".opencpq_html_class").html(html);
+	},
 
     display_dialog: function() {
         var self = this;
-        self.dialog = new Dialog(this, {
-            size: 'fullscreen',  // ToDo: Dialog widget erweitern, damits den case fullscreen auch gibt
+        self.dialog = new FullscreenDialog(this, {
             title: _t('Product Configuration'),
             buttons: [
                     {
-                        text: _t("Apply"),
+                        text: _t("OK"),
                         classes: 'btn-primary',
                         click: function () {
                             self.save_configuration();
@@ -54,33 +61,19 @@ var ConfigurationDialog = form_common.FormWidget.extend({
                     },
                 ],
             $content: QWeb.render('OpencpqConfigurator', {
-                "configurator": this.field_manager.get_field_value("configurator_type") || 0,
+                "configurator": this.field_manager.get_field_value("configurator_type") || "not_configurable",
+                "configuration_text": this.field_manager.get_field_value("configuration_text"),
+                "configuration_html": this.field_manager.get_field_value("configuration_html") || "(not configured)",
             }),
         }).open();
     },
 
-
     save_configuration: function() {
-        var configuration_html_local = $(".opencpq_input_html").val();
-        var configuration_text_local = $(".opencpq_input_text").val();
         this.field_manager.set_values({
-            "configuration_text": configuration_text_local,
-            //"configuration_html": "<h1>TEST</h1><div>tests</div>",
-            "configuration_html": configuration_html_local,
+            "configuration_text": $(".opencpq_input_html").val(),
+            "configuration_html": $(".opencpq_input_text").val(),
         });
     }
-
-    //Klappt soweit, aber Felder werden nur gesetzt, nicht gespeichert..
-    //  Das ist problematisch, wenn man configure_button auch im readonly-mode erlauben würde
-    //  Aber auch dann, wenn Zielfelder auch im edit-mode readonly sind(wie bei uns!)..dann speichert es die neuen werte nicht mit ab
-
-		/*
-		get iframe id
-		provide embedding API for the iframe
-		use get_field_value for the url_tag in the iframe
-		use set_field_value for the configuration_result
-		*/
-
 
 });
 
